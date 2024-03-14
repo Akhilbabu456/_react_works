df5ee9c91daeefcc0b27a80e98b5f63da049a778
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-03-14 23:30:04 +0530

    second commit

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,39 +1,105 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
-  
+   static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general"
+  }
+  static propsTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+   }
+
+
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=12967ab15b234a9b99b045a30c3ab584"
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=12967ab15b234a9b99b045a30c3ab584&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
-    let parseData = await data.json()
-    this.setState({articles: parseData.articles})
+    let parseData = await data.json();
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
   }
+  async componentDidMount() {
+   this.updateNews()
+  }
+
+  handleNextClick = async () => {
+      this.setState({page: this.state.page + 1})
+      this.updateNews()
+  };
+
+  handlePrevClick = async () => {
+    this.setState({page: this.state.page - 1})
+    this.updateNews()
+  };
 
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center" style={{margin: "40px 0px"}}>NewsMonkey - Top Headlines</h1>
+        
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
-              <div className="col col-xs-12 col-sm-6 col-md-4 col-lg-3" key={element.url}>
-                <NewsItem  title={element.title?element.title.slice(0,45): ""} description={element.description?element.description.slice(0,88): ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+              <div
+                className=" col-xs-12 col-sm-12 col-md-6 col-lg-4"
+                key={element.url}
+              >
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
               </div>
             );
           })}
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" className="btn btn-dark">Previous</button>
-        <button type="button" className="btn btn-dark">Next</button>
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  
   render() {
-   let {title, description, imageUrl, newsUrl} = this.props
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
-        <div className="card m-3" style={{width: "20rem"}}>
-          <img src={imageUrl? imageUrl:"https://static.toiimg.com/thumb/msid-108435839,imgsize-557227,width-400,resizemode-4/108435839.jpg"} className="card-img-top" alt="..." style={{backgroundSize: "cover", height: "12rem"}}/>
+        <div className="card m-3">
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://static.toiimg.com/thumb/msid-108435839,imgsize-557227,width-400,resizemode-4/108435839.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+            style={{ backgroundSize: "cover", height: "12rem" }}
+          />
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: "88%", zIndex: "1"}}>
+               {source}
+              </span>
           <div className="card-body">
-            <h5 className="card-title">{title? title: "He gets upset quickly’: Manohar Lal Khattar "}...</h5>
+            <h5 className="card-title">
+              {title ? title : "He gets upset quickly’: Manohar Lal Khattar "}..{" "}
+              
+            </h5>
             <p className="card-text">
-              {description? description: "Anil Vij, who served as home minister in Manohar Lal Khattar's government, was not prese"}...
+              {description
+                ? description
+                : "Anil Vij, who served as home minister in Manohar Lal Khattar's government, was not prese"}
+              ...
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}{" "}
+              </small>
             </p>
             <a href={newsUrl} target="_blank" className="btn btn-primary">
               Read More
