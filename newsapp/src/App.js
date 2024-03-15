f5d6abf1f4b68bc885343e52f9ffd9f1e64432cb
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-03-15 22:39:10 +0530

    third

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -14,7 +14,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize = 6
-  apiKey = process.env.REACT_APP_NEWS_API 
+  apiKey = (process.env.REACT_APP_NEWS_API).toString()
   state={
     progress: 0
   }
